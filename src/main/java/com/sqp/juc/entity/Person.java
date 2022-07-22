fb65b433e44d43f674030c9ce3782f93b94dd692
shanqingpeng <shanqingpeng0108@163.com>
2022-07-22 09:14:20 +0800

    Initial commit

---
+++ src/main/java/com/sqp/juc/entity/Person.java
@@ -0,0 +1,20 @@
+package com.sqp.juc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author shanqingpeng
+ * @date 2022/07/22
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private Integer id;
+
+    private String name;
+
+}
